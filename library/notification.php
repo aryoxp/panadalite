2c4346138d7e4dc0c6052a3cdce8b6000951a841
Aryo <aryoxp@gmail.com>
2013-09-19 09:44:59 +0700

    adding new notification library

---
+++ library/notification.php
@@ -0,0 +1,42 @@
+<?php
+
+class notification {
+    const all = -1;
+    const info = 0;
+    const warning = 1;
+    const error = 2;
+    const success = 3;
+
+    private $panada;
+
+    public function __construct( $panada ) {
+        $panada->session = new session($panada->config->session);
+        $this->panada = $panada;
+    }
+
+    public function add($type,$message){
+        $notification = $this->panada->session->get('notification');
+        if(!$notification) $notification = array();
+        array_push($notification, array($type,$message));
+        $this->panada->session->set('notification',$notification);
+    }
+
+    public function flush($type = self::all){
+        $notification = $this->panada->session->get('notification');
+
+        if($type == self::all) {
+            $this->panada->session->remove('notification');
+            return $notification;
+        }
+
+        $maintained = array();
+        $flushed = array();
+        foreach($notification as $n) {
+            if($n[0] == $type)
+                $flushed[] = $n[1];
+            else $maintained[] = $n;
+        }
+        $this->panada->session->set('notification',$maintained);
+        return $flushed;
+    }
+}
